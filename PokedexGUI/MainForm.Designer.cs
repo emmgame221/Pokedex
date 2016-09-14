30b6d93b9219cef0a08dcc8449da2508021ace02
Eric <Eric@ERIC-LAPTOP>
2016-09-14 17:51:00 -0400

    Continued working on GUI

--- PokedexGUI/MainForm.Designer.cs
+++ PokedexGUI/MainForm.Designer.cs
@@ -28,20 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.pokemonListComboBox = new System.Windows.Forms.ComboBox();
+            this.pokemonBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pokedexDataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pokemonListBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.hpLabel = new System.Windows.Forms.Label();
+            this.attackLabel = new System.Windows.Forms.Label();
+            this.defenseLabel = new System.Windows.Forms.Label();
+            this.spAttackLabel = new System.Windows.Forms.Label();
+            this.spDefenseLabel = new System.Windows.Forms.Label();
+            this.speedLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pokedexDataBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonListBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pokemonListComboBox
+            // 
+            this.pokemonListComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.pokemonListComboBox.FormattingEnabled = true;
+            this.pokemonListComboBox.Location = new System.Drawing.Point(12, 28);
+            this.pokemonListComboBox.Name = "pokemonListComboBox";
+            this.pokemonListComboBox.Size = new System.Drawing.Size(121, 24);
+            this.pokemonListComboBox.TabIndex = 0;
+            this.pokemonListComboBox.SelectedIndexChanged += new System.EventHandler(this.pokemonListComboBox_SelectedIndexChanged);
+            // 
+            // pokemonBindingSource
+            // 
+            this.pokemonBindingSource.DataSource = typeof(Pokedex.Pokemon);
+            // 
+            // pokedexDataBindingSource
+            // 
+            this.pokedexDataBindingSource.DataSource = typeof(Pokedex.PokedexData);
+            // 
+            // pokemonListBindingSource
+            // 
+            this.pokemonListBindingSource.DataMember = "PokemonList";
+            this.pokemonListBindingSource.DataSource = this.pokedexDataBindingSource;
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Location = new System.Drawing.Point(12, 69);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(40, 17);
+            this.typeLabel.TabIndex = 1;
+            this.typeLabel.Text = "Type";
+            // 
+            // hpLabel
+            // 
+            this.hpLabel.AutoSize = true;
+            this.hpLabel.Location = new System.Drawing.Point(158, 35);
+            this.hpLabel.Name = "hpLabel";
+            this.hpLabel.Size = new System.Drawing.Size(35, 17);
+            this.hpLabel.TabIndex = 3;
+            this.hpLabel.Text = "HP: ";
+            // 
+            // attackLabel
+            // 
+            this.attackLabel.AutoSize = true;
+            this.attackLabel.Location = new System.Drawing.Point(158, 69);
+            this.attackLabel.Name = "attackLabel";
+            this.attackLabel.Size = new System.Drawing.Size(55, 17);
+            this.attackLabel.TabIndex = 4;
+            this.attackLabel.Text = "Attack: ";
+            // 
+            // defenseLabel
+            // 
+            this.defenseLabel.AutoSize = true;
+            this.defenseLabel.Location = new System.Drawing.Point(158, 107);
+            this.defenseLabel.Name = "defenseLabel";
+            this.defenseLabel.Size = new System.Drawing.Size(69, 17);
+            this.defenseLabel.TabIndex = 5;
+            this.defenseLabel.Text = "Defense: ";
+            // 
+            // spAttackLabel
+            // 
+            this.spAttackLabel.AutoSize = true;
+            this.spAttackLabel.Location = new System.Drawing.Point(158, 143);
+            this.spAttackLabel.Name = "spAttackLabel";
+            this.spAttackLabel.Size = new System.Drawing.Size(80, 17);
+            this.spAttackLabel.TabIndex = 6;
+            this.spAttackLabel.Text = "Sp. Attack: ";
+            // 
+            // spDefenseLabel
+            // 
+            this.spDefenseLabel.AutoSize = true;
+            this.spDefenseLabel.Location = new System.Drawing.Point(158, 178);
+            this.spDefenseLabel.Name = "spDefenseLabel";
+            this.spDefenseLabel.Size = new System.Drawing.Size(94, 17);
+            this.spDefenseLabel.TabIndex = 7;
+            this.spDefenseLabel.Text = "Sp. Defense: ";
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.AutoSize = true;
+            this.speedLabel.Location = new System.Drawing.Point(158, 213);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(57, 17);
+            this.speedLabel.TabIndex = 8;
+            this.speedLabel.Text = "Speed: ";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(909, 518);
+            this.ClientSize = new System.Drawing.Size(314, 329);
+            this.Controls.Add(this.speedLabel);
+            this.Controls.Add(this.spDefenseLabel);
+            this.Controls.Add(this.spAttackLabel);
+            this.Controls.Add(this.defenseLabel);
+            this.Controls.Add(this.attackLabel);
+            this.Controls.Add(this.hpLabel);
+            this.Controls.Add(this.typeLabel);
+            this.Controls.Add(this.pokemonListComboBox);
             this.Name = "MainForm";
             this.Text = "Pokedex";
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pokedexDataBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pokemonListBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox pokemonListComboBox;
+        private System.Windows.Forms.BindingSource pokemonListBindingSource;
+        private System.Windows.Forms.BindingSource pokedexDataBindingSource;
+        private System.Windows.Forms.BindingSource pokemonBindingSource;
+        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.Label hpLabel;
+        private System.Windows.Forms.Label attackLabel;
+        private System.Windows.Forms.Label defenseLabel;
+        private System.Windows.Forms.Label spAttackLabel;
+        private System.Windows.Forms.Label spDefenseLabel;
+        private System.Windows.Forms.Label speedLabel;
     }
 }
 
--- PokedexGUI/MainForm.cs
+++ PokedexGUI/MainForm.cs
@@ -16,12 +16,44 @@ namespace PokedexGUI
     public partial class MainForm : Form
     {
         PokedexData pokedex;
-        const string DataFileName = "pokedex.json";
+        Pokemon currentPokemon;
+        const string DataFileName = @"C:\Users\Eric\Documents\Visual Studio 2015\Projects\Pokedex\GeneratePokedexData\bin\Debug\pokedex.json";
 
         public MainForm()
         {
             pokedex = JsonConvert.DeserializeObject<PokedexData>(File.ReadAllText(DataFileName));
+            currentPokemon = pokedex.PokemonList[0];
             InitializeComponent();
+            BindComboBoxes();
+        }
+
+        void BindComboBoxes()
+        {
+            pokemonListComboBox.DataSource = pokedex.PokemonList;
+            pokemonListComboBox.DisplayMember = "Name";
+        }
+
+        void UpdateDisplay()
+        {
+            typeLabel.Text = currentPokemon.Type1.ToString();
+            if(currentPokemon.Type2 != PokemonType.None)
+            {
+                typeLabel.Text += " / " + currentPokemon.Type2.ToString();
+            }
+            hpLabel.Text = string.Format("HP: {0}", currentPokemon.BaseHP);
+            attackLabel.Text = string.Format("Attack: {0}", currentPokemon.BaseAttack);
+            defenseLabel.Text = string.Format("Defense: {0}", currentPokemon.BaseDefense);
+            spAttackLabel.Text = string.Format("Sp. Attack: {0}", currentPokemon.BaseSpecialAttack);
+            spDefenseLabel.Text = string.Format("Sp. Defense: {0}", currentPokemon.BaseSpecialDefense);
+            speedLabel.Text = string.Format("Speed: {0}", currentPokemon.BaseSpeed);
+        }
+
+        private void pokemonListComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Pokemon newPokemon = (pokedex.PokemonList.Find(p => p.Name == pokemonListComboBox.Text));
+            if (newPokemon != null)
+                currentPokemon = newPokemon;
+            UpdateDisplay();
         }
     }
 }
